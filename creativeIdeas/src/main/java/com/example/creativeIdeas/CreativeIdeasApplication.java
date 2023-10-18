d45bf708ca19b673b52a3840bf56387b5cc071a2
Ivan Welborn <113381087+iwelbo00@users.noreply.github.com>
2023-10-17 20:07:59 -0500

    beginning of my demise

---
+++ creativeIdeas/src/main/java/com/example/creativeIdeas/CreativeIdeasApplication.java
@@ -0,0 +1,13 @@
+package com.example.creativeIdeas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativeIdeasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreativeIdeasApplication.class, args);
+	}
+
+}
---
+++ creativeIdeas/src/test/java/com/example/creativeIdeas/CreativeIdeasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creativeIdeas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreativeIdeasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
